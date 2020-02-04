23fd71bdcdea74a788a125c0ec1d54f45922e961
Aysam Farhoud <aysamfar11@gmail.com>
2020-02-04 17:09:32 +0200

    adds handling city and hostels

--- public/index.js
+++ public/index.js
@@ -1,4 +1,7 @@
 const form = document.querySelector('form');
+const cityName = document.querySelector('#cityInput')
+const DOA = document.querySelector('#DOAInput')
+const DOD = document.querySelector('#DODInput')
 
 function request(url,cb){
     var xhr = new XMLHttpRequest();
@@ -9,7 +12,6 @@ function request(url,cb){
             cb('error',xhr.responseType)
         }
     };
-
     xhr.open('GET',url,true);
     xhr.send();
 }
@@ -19,16 +21,13 @@ function updateDom(err,data){
         console.log(err);
     }else{
         var inputData = JSON.parse(data);
-        
     }
 }
-document.onload = function(){
-    console.log("inside on load");
-}
+
+const url = '/search?city='+cityName.value+'&arrival='+DOA.value+'&departure='+DOD.value;
 
 document.addEventListener('submit',(e)=>{
     e.preventDefault()
-    console.log("inside event listener");
-    request('/getHostels', updateDom);
+    request(url, updateDom);
 })
 
