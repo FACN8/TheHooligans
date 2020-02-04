7ac5288b07351fcd072b6125cb6494338e29cb6b
Aysam Farhoud <aysamfar11@gmail.com>
2020-02-04 14:42:38 +0200

    adds server

---
+++ src/handlers.js
@@ -0,0 +1,48 @@
+const { readFile } = require("fs");
+const path = require("path");
+const getData = require("./queries/getData.js");
+const dbConnection = require('./database/db_connection')
+const querystring = require('querystring')
+
+const serverError = (err, response) => {
+    response.writeHead(500, "Content-Type:text/html");
+    response.end("<h1>Sorry, there was a problem loading the page</h1>");
+    console.log(err);
+  };
+  
+  const homeHandler = response => {
+    const filepath = path.join(__dirname, "..", "public", "index.html");
+    readFile(filepath, (err, file) => {
+      if (err) return serverError(err, response);
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end(file);
+    });
+  };
+
+  const publicHandler = (url, response) => {
+    const filepath = path.join(__dirname, "..", url);
+    readFile(filepath, (err, file) => {
+      if (err) return serverError(err, response);
+      const [, extension] = url.split(".");
+      const extensionType = {
+        html: "text/html",
+        css: "text/css",
+        js: "application/javascript",
+        ico: "image/x-icon"
+      };
+      response.writeHead(200, { "content-type": extensionType[extension] });
+      response.end(file);
+    });
+  };
+
+  const errorHandler = response => {
+    response.writeHead(404, { "content-type": "text/html" });
+    response.end("<h1>404 Page Requested Cannot be Found</h1>");
+  };
+
+  module.exports = {
+    homeHandler,
+    publicHandler,
+    errorHandler,
+  };
+  
