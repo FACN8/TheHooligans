67068e7cd8975768d91f0834cb376c117e74a637
92yo <56390606+92yo@users.noreply.github.com>
2020-02-04 17:17:40 +0200

    Revert "Inserts Files into tables, Adds header to index.html and div-container "

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,75 +1,34 @@
 BEGIN;
 
-DROP TABLE IF EXISTS city,
-hostel,
-reservation,
-guest;
-
-CREATE TABLE city (
-  id SERIAL PRIMARY KEY,
-  name varchar(100) NOT NULL
+DROP TABLE IF EXISTS city, hostel, reservation, guest;
+CREATE TABLE "city" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar(100) NOT NULL
 );
 
-CREATE TABLE hostel (
-  id SERIAL PRIMARY KEY,
-  name varchar(100) NOT NULL,
-  capacity integer,
-  description varchar(100),
-  contact_info varchar(100),
-  city_id integer
+CREATE TABLE "hostel" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar(100) NOT NULL,
+  "capacity" integer,
+  "description" varchar(100),
+  "contact_info" varchar(100),
+  "city_id" integer
 );
 
-CREATE TABLE guest (
-  id SERIAL PRIMARY KEY,
-  name varchar(100) NOT NULL,
-  hostel_id varchar(100) NOT NULL,
-  phoneNumber varchar(100) NOT NULL
+CREATE TABLE "guest" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar(100) NOT NULL,
+  "hostel_id" varchar(100) NOT NULL,
+  "phoneNumber" integer
 );
 
-CREATE TABLE reservation (
-  id SERIAL PRIMARY KEY,
-  hostel_id integer,
-  guest_id integer,
-  date_of_arrival date,
-  date_of_departure date
+CREATE TABLE "reservation" (
+  "id" SERIAL PRIMARY KEY,
+  "hostel_id" integer,
+  "guests_id" integer,
+  "date_of_arrival" date,
+  "date_of_departure" date,
+  "guest_id" integer
 );
-
-INSERT INTO
-  reservation (
-    hostel_id,
-    guest_id,
-    date_of_arrival,
-    date_of_departure
-  )
-VALUES
-  (1, 1, '04-Feb-20', '05-FEB-20');
-
-INSERT INTO
-  guest (name, hostel_id, phoneNumber)
-VALUES
-  ('Aysam', 1, '+972-549203843');
-
-INSERT INTO
-  hostel (
-    name,
-    capacity,
-    description,
-    contact_info,
-    city_id
-  )
-VALUES
-  (
-    'Haifa Port',
-    200,
-    'Hostel by the water',
-    '+972-54938293',
-    1
-  )
-  ;
-
-INSERT INTO
-  city (name)
-VALUES
-  ('Haifa');
 
 COMMIT;
